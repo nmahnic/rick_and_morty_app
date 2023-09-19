8c4450a6f588a2e3f8cc82a4a0a65afebb0676c1
Nicolas Mahnic <nico.mahnic.ar@gmail.com>
2023-09-19 17:11:43 +0200

    HomeScreen

--- product/src/main/java/com/nicomahnic/product/HomeScreen.kt
+++ product/src/main/java/com/nicomahnic/product/HomeScreen.kt
@@ -1,16 +1,62 @@
 package com.nicomahnic.product
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
-    onClick: () -> Unit
+    onClick: () -> Unit,
 ) {
-    Button(
-        onClick = onClick
-    ){
-        Text("HOME SCREEN")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("App for test") }
+            )
+        },
+        bottomBar = {
+            BottomAppBar {
+                Box(
+                    modifier = Modifier.padding(horizontal = 20.dp)
+                ){
+                    Text(
+                        text = "by Nicolás Mahnic",
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.End
+                    )
+                }
+            }
+        }
+    ) { contentPadding ->
+        Column(
+            modifier = Modifier
+                .padding(contentPadding)
+                .fillMaxWidth()
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(
+                onClick = onClick
+            ) {
+                Text("Start")
+            }
+        }
     }
 }
