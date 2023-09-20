1e16f2d8ca1706f68777d27b543921631fda978c
Nicolas Mahnic <nico.mahnic.ar@gmail.com>
2023-09-20 09:09:53 +0200

    fix repository injection

--- data/src/main/java/com/nicomahnic/data/di/RepositoriesModules.kt
+++ data/src/main/java/com/nicomahnic/data/di/RepositoriesModules.kt
@@ -1,12 +1,15 @@
 package com.nicomahnic.data.di
 
 import com.nicomahnic.data.repository.CharactersRepositoryImpl
+import com.nicomahnic.domain.repository.CharactersRepository
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 object RepositoriesModules {
 
     val useCases = module {
-        single { CharactersRepositoryImpl( get() ) }
+        singleOf(::CharactersRepositoryImpl){ bind<CharactersRepository>() }
     }
 
 }
